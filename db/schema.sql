5d2fdf726d90d8649edb87381d6ace4ed4a3a515
byigitt <c2228009@student.cankaya.edu.tr>
2024-10-29 00:48:44 +0300

    feat/db-schema: change schema due to s3

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE one_time_links (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    file_name VARCHAR(255) NOT NULL,
-    file_content BYTEA NOT NULL,
-    email_to VARCHAR(255) NOT NULL,
-    email_from VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    file_name VARCHAR(255),
+    file_key VARCHAR(255),
+    email_to VARCHAR(255),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
     downloaded BOOLEAN DEFAULT FALSE,
